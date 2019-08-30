329580326240e66f3d2760352bac03addfbe7a32
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2019-08-30 05:15:24 +0500

    Update Version.pas

--- Version.pas
+++ Version.pas
@@ -2,7 +2,7 @@
 
 type
   Version = static class
-    private static fversion := new System.Version(1, 0, 0, 22);
+    private static fversion := new System.Version(1, 0, 0, 36);
     
     public static property Version: System.Version read fversion;
   end;
